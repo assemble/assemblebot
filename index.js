f22a2737b5c90fbac436cad058250024fdf4b786
Brian Woodward <brian.woodward@gmail.com>
2015-12-23 19:42:10 -0500

    update main module file to create an AssembleBot class inherited from GithubBot and use the githubbot-connect plugin to create a middleware method

--- index.js
+++ index.js
@@ -7,57 +7,19 @@
 
 'use strict';
 
-/**
- * webtask.io task to handle github webhooks
- */
+var GithubBot = require('githubbot');
+var middleware = require('githubbot-connect');
+var handlers = require('./src/handlers');
 
-var async = require('async');
-var post = require('./lib/post-comment');
-var error = require('./lib/error');
-var config = require('./lib/config');
-var render = require('./lib/render');
-var success = require('./lib/success');
-var template = require('./lib/template');
-
-module.exports = function (context, req, res) {
-  var payload = context.data;
-  if (payload.action !== 'opened') {
-    return success(res, {
-      code: 200,
-      status: 'success',
-      action: payload.action,
-      message: 'No action taken'
-    });
+function AssembleBot(options) {
+  if (!(this instanceof AssembleBot)) {
+    return new AssembleBot(options);
   }
+  GithubBot.call(this, options);
+  this.use(middleware());
+  this.use(handlers(this.options));
+}
 
-  if (!payload.GITHUB_TOKEN) {
-    return error(res, 'Invalid GITHUB_TOKEN');
-  }
+GithubBot.extend(AssembleBot);
 
-  var token = payload.GITHUB_TOKEN;
-  delete payload.GITHUB_TOKEN;
-
-  // options to use when pulling down files from this repo.
-  var repoOpts = {
-    token: token,
-    owner: 'assemble',
-    repo: 'assemblebot'
-  };
-
-  // options used to post comments
-  var commentOpts = {
-    owner: payload.repository.owner.login,
-    repo: payload.repository.name,
-    number: payload.issue.number
-  };
-
-  async.waterfall([
-    config(payload, repoOpts),
-    template(repoOpts),
-    render(payload),
-    post(token, commentOpts),
-  ], function(err, results) {
-    if (err) return error(res, err);
-    success(res, results);
-  });
-};
+module.exports = AssembleBot;
