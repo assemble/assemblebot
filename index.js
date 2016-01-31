b9ee4978e378dfcd43c231df17771c954a986756
Brian Woodward <brian.woodward@gmail.com>
2016-01-31 18:57:01 -0500

    adds `assemble` property to the bot instance and configures defaults

--- index.js
+++ index.js
@@ -8,14 +8,15 @@
 'use strict';
 
 var GithubBot = require('githubbot');
-var handlers = require('./src/handlers');
+var handlers = require('./lib/handlers');
+var utils = require('./lib/utils');
 
 /**
- * Main class for creating a new AssembleBot instance. This bot extends [GithubBot][githubbot] and adds
- * sepcific handlers for responding to [assemble][assemble] issues.
+ * Main class for creating a new AssembleBot instance. This bot extends [GithubBot][githubbot]
+ * and creates an instance of [assemble-core][] that can be used to render response templates.
  *
  * ```js
- * var bot = new AssembleBot({GITHUB_TOKEN: 'XXX'});
+ * var bot = new AssembleBot({GITHUB_TOKEN: 'xxxxxxxxxxxxxxx'});
  * ```
  *
  * @param {Object} `options` Options to use to configure the bot.
@@ -28,6 +29,7 @@ function AssembleBot(options) {
     return new AssembleBot(options);
   }
   GithubBot.call(this, options);
+  this.initAssemble();
   this.use(handlers(this.options));
 }
 
@@ -36,6 +38,23 @@ function AssembleBot(options) {
  */
 
 GithubBot.extend(AssembleBot);
+
+/**
+ * Create an instance of [assemble-core] with some configured defaults:
+ *
+ *  - uses [assemble-loader]
+ *  - uses [engine-handlebars] for ".hbs" files.
+ *  - uses [handlebars-helpers]
+ *  - uses [helper-issue]
+ */
+
+AssembleBot.prototype.initAssemble = function() {
+  this.define('assemble', new utils.Assemble());
+  this.assemble.use(utils.loader());
+  this.assemble.engine('hbs', utils.engine);
+  this.assemble.helpers(utils.helpers());
+  this.assemble.helper('issue', utils.issue);
+};
 
 /**
  * Exposes `AssembleBot`
